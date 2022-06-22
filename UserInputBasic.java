ee42bf3893e4bad9079a5548b90f3293cec9ec70
Ritam Das <106969260+ritam332@users.noreply.github.com>
2022-06-22 18:29:05 +0530

    Update UserInputBasic.java

--- UserInputBasic.java
+++ UserInputBasic.java
@@ -1,23 +1,23 @@
 public class hello {
 
-	 public static void main(String[] args) {
-		Scanner scanner= new Scanner(System.in);
-				System.out.println("Enter Your year of birth : ");
-		boolean hasNextInt=scanner.hasNextInt();
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Your year of birth : ");
+		boolean hasNextInt = scanner.hasNextInt();
 		if (hasNextInt) {
-			int yearOfBirth=scanner.nextInt();
+			int yearOfBirth = scanner.nextInt();
 			scanner.nextLine();
 			System.out.println("eNTER yoUR name ");
-			String name=scanner.nextLine();
-			int age=2022-yearOfBirth;
-			
-			if (age>=0 && age<=100) {
-				System.out.println("Your Name Is "+name+" and your age is "+age);
-				
-			}else {
+			String name = scanner.nextLine();
+			int age = 2022 - yearOfBirth;
+
+			if (age >= 0 && age <= 100) {
+				System.out.println("Your Name Is " + name + " and your age is " + age);
+
+			} else {
 				System.out.println("Invalid Input");
 			}
-		}else {
+		} else {
 			System.out.println("Unable to parse year of birth");
 		}
 		scanner.close();
